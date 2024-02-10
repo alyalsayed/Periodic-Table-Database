cc22f3f4046df8df80468d855c0a7b15e8214b78
alyalsayed <alyalsayed56@yahoo.com>
2024-02-10 17:04:31 +0000

    feat: update README.md

--- element.sh
+++ element.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
+# if no argument is provided
 if [ $# -eq 0 ]; then
     echo "Please provide an element as an argument."
 fi
+
+# Define PSQL variable for querying the database
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
