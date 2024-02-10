4fc83b03000d975aa1bd1b06a03d51d857d5217d
alyalsayed <alyalsayed56@yahoo.com>
2024-02-10 18:39:19 +0000

    fix: fix string args in terminal

--- element.sh
+++ element.sh
@@ -7,8 +7,14 @@ PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 if [ $# -eq 0 ]; then
     echo "Please provide an element as an argument."
 else
-    # Fetch information about the element from the database
-    element_info=$($PSQL "SELECT elements.atomic_number, elements.symbol, elements.name, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM elements JOIN properties ON elements.atomic_number = properties.atomic_number JOIN types ON properties.type_id = types.type_id WHERE elements.atomic_number = '$1' OR elements.symbol = '$1' OR elements.name = '$1';")
+    # Check if the argument is a valid atomic number
+    if [[ "$1" =~ ^[0-9]+$ ]]; then
+        # Fetch information about the element using atomic number
+        element_info=$($PSQL "SELECT elements.atomic_number, elements.symbol, elements.name, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM elements JOIN properties ON elements.atomic_number = properties.atomic_number JOIN types ON properties.type_id = types.type_id WHERE elements.atomic_number = $1;")
+    else
+        # Fetch information about the element using symbol or name
+        element_info=$($PSQL "SELECT elements.atomic_number, elements.symbol, elements.name, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius FROM elements JOIN properties ON elements.atomic_number = properties.atomic_number JOIN types ON properties.type_id = types.type_id WHERE elements.symbol = '$1' OR elements.name = '$1';")
+    fi
 
     # Check if element information is retrieved
     if [ -n "$element_info" ]; then
